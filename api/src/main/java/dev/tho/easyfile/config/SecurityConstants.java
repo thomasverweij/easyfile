086d005f6575ee96e8fae55d89c823736a15b303
Thomas Verweij <tverweij@pm.me>
2021-06-06 15:54:57 +0200

    fix jwt settings

--- api/src/main/java/dev/tho/easyfile/config/SecurityConstants.java
+++ api/src/main/java/dev/tho/easyfile/config/SecurityConstants.java
@@ -1,7 +1,12 @@
 package dev.tho.easyfile.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
+
+    @Value("${jwt.accesskey}")
+    public static  String SECRET;
+
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
